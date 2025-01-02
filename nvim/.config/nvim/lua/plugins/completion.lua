1d46c2d414569ed55bc3bf6f59b31e8e78b2bd28
diego.perezurbina <diego.p.urbina@gmail.com>
2025-01-02 15:56:03 +0100

    add nvim config

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,24 @@
+return {
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+
+    version = "*",
+
+    opts = {
+        -- <C-y> to accept, <C-p> and <C-n> to move
+        keymap = { preset = "default" },
+
+        appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = "mono"
+        },
+
+        sources = {
+            default = { "lsp", "path", "snippets" },
+        },
+        cmdline = {
+            enabled = false,
+        }
+    },
+    opts_extend = { "sources.default" }
+}
---
+++ nvim/.config/nvim/lua/plugins/highlight.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "echasnovski/mini.cursorword",
+        version = "*",
+        config = function()
+            require("mini.cursorword").setup()
+            --vim.cmd("hi! link MiniCursorword Visual")
+            --vim.cmd("hi! MiniCursorwordCurrent gui=nocombine guifg=NONE guibg=NONE")
+        end
+    },
+    {
+        'fei6409/log-highlight.nvim',
+        config = function()
+            require('log-highlight').setup {}
+        end,
+    }
+}
